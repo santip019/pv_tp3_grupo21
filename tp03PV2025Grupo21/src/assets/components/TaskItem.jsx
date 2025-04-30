0d097ad35aacca8387f8913e7e368449bfb36108
Santi Padilla <smauriciop001@gmail.com>
2025-04-30 01:35:11 -0300

    fix: correccion de estructuracion de prop TaskItem [Cruz Antonella, Efmimov Valentina, Padilla Santiago]

---
+++ tp03PV2025Grupo21/src/assets/components/TaskItem.jsx
@@ -0,0 +1,21 @@
+function TaskItem({ task }) {
+    return (
+      <li id={task?.id} className="todo_item">
+        <button className="items_check">
+          <svg>
+            <circle cx="11.998" cy="11.998" fillRule="nonzero" r="9.998" />
+          </svg>
+          <p>{task?.title}</p>
+        </button>
+        <div className="items_izquierda">
+          <button>
+            <span className="TextoOculto">Eliminar</span>
+            <svg>
+              <path d="" />
+            </svg>
+          </button>
+        </div>
+      </li>
+    );
+  } // define los elementos de la lista de tareas
+  export default TaskItem; // exporta el componente TaskItem para que pueda ser utilizado en otros archivos
