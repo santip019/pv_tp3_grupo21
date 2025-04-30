1b9e5c5d8eb2289593e689c473c860be54812e17
Antonella Guadalupe Cruz <antoguadalupecruz@gmail.com>
2025-04-30 02:23:58 -0300

    fix: Habilitar el renderizado de tareas [ Cruz Antonella, Efimov Valentina, Padilla Santiago ]

--- tp03PV2025Grupo21/src/assets/components/Tarea.jsx
+++ tp03PV2025Grupo21/src/assets/components/Tarea.jsx
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Formulario from "./TaskInput";
 import TaskList from "./TaskList";
 
 function Home() {
+  const [tasks, setTasks] = useState([]);
+
+  const agregarTarea = (tarea) => {
+    const nuevaTarea = {
+      id: Date.now(),
+      title: tarea,
+    };
+    setTasks([...tasks, nuevaTarea]);
+  };
+
+  const eliminarTarea = (id) => {
+    setTasks(tasks.filter(task => task.id !== id));
+  };
+
   return (
     <div className="wrapper">
-      <Formulario />
-      <TaskList tasks={[]} />
+      <Formulario onAgregar={agregarTarea} />
+      <TaskList tasks={tasks} onEliminar={eliminarTarea} />
     </div>
   );
 }
+
 export default Home;
