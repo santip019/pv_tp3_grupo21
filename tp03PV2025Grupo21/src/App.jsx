f8a330fb77b94ef1ab1647d3cf4084f46ea3e679
maxiicalpa <maxiicalpa1gmail.com>
2025-04-30 14:16:33 -0300

    Se agrego Arreglo.jsx y Producto.jsx - Calpanchay Maximiliano

--- tp03PV2025Grupo21/src/App.jsx
+++ tp03PV2025Grupo21/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Home from './assets/components/Tarea.jsx'
+import Producto from './assets/components/Producto.jsx';
 
 function App() {
   return (
     <div className="container">
       <h1 className="title">Lista de tareas</h1>
+        <Producto />
         <Home />
     </div>
   )
